d20decb1e5f25b0f090265c86dc68e5e5324c62b
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2022-10-15 20:27:00 -0300

    feat: Made the snake grow

--- game.cpp
+++ game.cpp
@@ -7,10 +7,41 @@
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
+class Fruit {
+public:
+	SDL_Rect rect;
+
+	Fruit() {
+		rect.w = 20;
+		rect.h = 20;
+
+		change_position();
+	}
+
+	void update(double delta) {
+		
+	}
+
+	void draw(SDL_Renderer* renderer) {
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &rect);
+	}
+
+	int change_position() {
+		rect.x = rand() % (SCREEN_WIDTH - 20);
+		rect.y = rand() % (SCREEN_HEIGHT - 20);
+	}
+};
 
 class SnakeNode {
 public:
 	SDL_Rect rect;
+	SnakeNode* node_in_front = nullptr;
+
+	SnakeNode() {
+		rect.w = 20;
+		rect.h = 20;
+	};
 
 	SnakeNode(int x, int y) {
 		rect.x = x;
@@ -19,9 +50,8 @@ public:
 		rect.h = 20;
 	}
 
-	void update() {
-
-	}
+	void update(double delta) {
+			}
 
 	void draw(SDL_Renderer* renderer) {
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
@@ -52,51 +82,64 @@ public:
 	}
 
 	void update(double delta) {
-		for (auto& node : nodes) {
-			if (direction == RIGHT) node.rect.x += speed * delta;
-			else if (direction == LEFT) node.rect.x -= speed * delta;
-			else if (direction == UP) node.rect.y -= speed * delta;
-			else if (direction == DOWN) node.rect.y += speed * delta;
-
-			node.update();
-			stick_to_screen();
-		}
+		SnakeNode& head = nodes.front();
+		if (direction == RIGHT) head.rect.x += speed * delta;
+		else if (direction == LEFT) head.rect.x -= speed * delta;
+		else if (direction == UP) head.rect.y -= speed * delta;
+		else if (direction == DOWN) head.rect.y += speed * delta;
+		nodes.push_front(head);
+		nodes.pop_back();
+	
+		stick_to_screen();
 	}
-
 	void draw(SDL_Renderer* renderer) {
 		for (auto& node : nodes) {
 			node.draw(renderer);
 		}
 	}
 
+	void append_node() {
+		SnakeNode& back = nodes.back();
+
+		SnakeNode new_node;
+
+		if (direction == RIGHT) {
+			new_node.rect.x = back.rect.x - back.rect.w - offset;
+			new_node.rect.y = back.rect.y;
+		} else if (direction == LEFT) {
+			new_node.rect.x = back.rect.x + back.rect.w + offset;
+			new_node.rect.y = back.rect.y;
+		} else if (direction == UP) {
+			new_node.rect.x = back.rect.x;
+			new_node.rect.y = back.rect.y + back.rect.h + offset;
+		} else if (direction == DOWN) {
+			new_node.rect.x = back.rect.x;
+			new_node.rect.y = back.rect.y - back.rect.h - offset;
+		}
+
+		nodes.push_back(new_node);
+	}
+
 	void change_direction(int new_direction) {
+		if (
+			direction == RIGHT && new_direction == LEFT ||
+			direction == LEFT && new_direction == RIGHT ||
+			direction == UP && new_direction == DOWN ||
+			direction == DOWN && new_direction == UP
+		) {
+			return;
+		}
 		direction = new_direction;
 	}
-};
 
-class Fruit {
-public:
-	SDL_Rect rect;
+	bool has_collided_with_fruit(Fruit fruit) {
+		SnakeNode& head = nodes.front();
 
-	Fruit() {
-		rect.w = 20;
-		rect.h = 20;
+		if (SDL_HasIntersection(&head.rect, &fruit.rect)) {
+			return true;
+		}
 
-		change_position();
-	}
-
-	void update(double delta) {
-		
-	}
-
-	void draw(SDL_Renderer* renderer) {
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-		SDL_RenderFillRect(renderer, &rect);
-	}
-
-	int change_position() {
-		rect.x = rand() % (SCREEN_WIDTH - 20);
-		rect.y = rand() % (SCREEN_HEIGHT - 20);
+		return false;
 	}
 };
 
@@ -157,6 +200,11 @@ int main(int argc, char* argv[]) {
 		snake.draw(renderer);
 
 		fruit.draw(renderer);
+
+		if (snake.has_collided_with_fruit(fruit)) {
+			snake.append_node();
+			fruit.change_position();
+		}
 
 
 		SDL_RenderPresent(renderer);
